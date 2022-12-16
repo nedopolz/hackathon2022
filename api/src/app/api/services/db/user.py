34ee91478c956c31967c950b1a2b109c8076affa
Bushmester <hibushland@gmail.com>
2022-12-16 23:46:43 +0300

    Add UserService

---
+++ api/src/app/api/services/db/user.py
@@ -0,0 +1,24 @@
+from functools import lru_cache
+
+from src.app.api.models.models import User
+from src.app.db import database
+
+
+class UserService:
+    def __init__(self):
+        self.database = database
+
+    async def get_user_by_tg_id(self, tg_id: str):
+        query = User.__table__.select().where(User.tg_id == tg_id)
+        user = await self.database.fetch_one(query)
+        return user
+
+    async def create_user(self, data: dict):
+        query = User.__table__.insert().values(**data)
+        user = await self.database.execute(query)
+        return user
+
+
+@lru_cache()
+def get_user_db_service():
+    return UserService()
