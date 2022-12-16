61c69d0b5f9ca60bb6ae817810c0a12024b464cc
Bushmester <hibushland@gmail.com>
2022-12-17 00:58:50 +0300

    Add get_questions()

--- api/src/app/api/routers/questions.py
+++ api/src/app/api/routers/questions.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.schemas.question import QuestionAndHisAnswer
+from api.services.db.questions import get_questions_service
+from api.services.db.session import get_session
 
 router = APIRouter(prefix="/questions", tags=["Questions"])
 
 
-@router.get("/", description="Получить список вопросов для пользователя")
-async def get_questions():
-    pass
+@router.get("/", description="Получить список вопросов для пользователя", response_model=list[QuestionAndHisAnswer])
+async def get_questions(
+    question_service=Depends(get_questions_service), session: AsyncSession = Depends(get_session)
+):
+    questions = await question_service.get_question(session)
+    return questions
