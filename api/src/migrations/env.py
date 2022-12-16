b3f7c2400d6be57cf8d51df6d9b52ffb0051cc8d
Bushmester <hibushland@gmail.com>
2022-12-16 15:32:23 +0300

    Rename from user to models

--- api/src/migrations/env.py
+++ api/src/migrations/env.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 
-from app.api.models.user import metadata
+from app.api.models.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
