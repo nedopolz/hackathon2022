375f839c0dfe17e4cbe89d8393d508a268a69a5f
RakhmatullinAK <rakhmatullinak@akbarsdigital.ru>
2022-12-17 03:12:52 +0400

    add calculators

---
+++ api/src/calculations/portfilio_risk_calc.py
@@ -0,0 +1,54 @@
+
+class PortfolioRisk:
+    @staticmethod
+    def get_investment_horizon_index(investment_horizon: int) -> float:
+        if investment_horizon < 1:
+            return 0.3
+        elif investment_horizon < 3:
+            return 0.2
+        elif investment_horizon < 5:
+            return 0.15
+        elif investment_horizon < 10:
+            return 0.1
+        return 0
+
+    @staticmethod
+    def get_obligations_index(obligations: bool) -> float:
+        if obligations:
+            return -0.2
+        return 0.0
+
+    @staticmethod
+    def get_age_index(age: int) -> float:
+        if age < 30:
+            return 0.2
+        elif age < 40:
+            return 0.15
+        elif age < 50:
+            return 0.1
+        return 0
+
+    @staticmethod
+    def get_goal_index(goal: str) -> float:
+        if goal == "Медленное накопление":
+            return 0.3
+        elif goal == "Умеренное накопление ":
+            return 0.4
+        elif goal == "Максимальная доходность":
+            return 0.5
+
+    def calculate(self, portfolio_id, answers: dict):
+        investment_horizon = int(answers.get("investment_horizon"))
+        age = answers.get("age")
+        goal = answers.get("goal")
+        obligations = answers.get("obligations")
+        risk_tolerance_index = 0
+        risk_tolerance_index += self.get_investment_horizon_index(investment_horizon)
+        risk_tolerance_index += self.get_obligations_index(obligations)
+        risk_tolerance_index += self.get_age_index(age)
+        risk_tolerance_index += self.get_goal_index(goal)
+        return risk_tolerance_index
+
+
+pr = PortfolioRisk()
+print(pr.calculate(1, {"investment_horizon": 1, "age": 38, "goal": "Медленное накопление", "obligations": False}))
