d3cdff0966e32bcacb07cbb0f3c2711fac897b00
Bushmester <hibushland@gmail.com>
2022-12-17 02:52:59 +0300

    Add get_portfolio()

--- api/src/app/api/routers/portfolio.py
+++ api/src/app/api/routers/portfolio.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.services.db.potrfolio import get_portfolio_db_service
+from api.services.db.session import get_session
 
 router = APIRouter(prefix="/portfolio", tags=["Portfolio"])
 
 
-@router.get("/", description="Получить портфель пользователя")
-async def get_portfolio():
-    pass
+@router.get("/{user_id}", description="Получить портфель пользователя")
+async def get_portfolio(
+        user_id: int, portfolio_service=Depends(get_portfolio_db_service), session: AsyncSession = Depends(get_session)
+):
+    portfolios = await portfolio_service.get_portfolio(user_id, session)
+    return portfolios
 
 
 @router.post("/", description="Создать портфель пользователя")
